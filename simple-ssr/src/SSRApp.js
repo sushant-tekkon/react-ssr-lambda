8313cce5ed5402882ebe2cc3af8273b0ee5d1206
Sushant Adhikari <sushant@tekkon.com.np>
2022-07-07 10:44:12 +0545

    added form code

--- simple-ssr/src/SSRApp.js
+++ simple-ssr/src/SSRApp.js
@@ -5,9 +5,35 @@ import React from "react";
 
 const SSRApp = () => {
   return (
-    <div>
-      <h1>Server side rendering working</h1>
-    </div>
+    <form>
+      <input name="email" className="form-control mb-1" placeholder="Email" />
+
+      <input
+        name="password"
+        placeholder="Password"
+        className="form-control mb-1"
+        type="password"
+      />
+
+      <div className="form-group text-center">
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            id={"rememberMe"}
+            name={"rememberMe"}
+          />
+
+          <label className="form-check-label" htmlFor={"rememberMe"}>
+            Remember Me
+          </label>
+        </div>
+      </div>
+
+      <button className="btn btn-primary btn-block" type="submit">
+        Sign In
+      </button>
+    </form>
   );
 };
 
