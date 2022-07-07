322b05191a9f3e39e4d94ca180b918a09e65dffa
Sushant Adhikari <sushant@tekkon.com.np>
2022-07-07 12:00:36 +0545

    server side rendering works

--- simple-ssr/src/App.js
+++ simple-ssr/src/App.js
@@ -2,34 +2,13 @@
 // SPDX-License-Identifier: MIT-0
 
 import React, { useEffect, useState } from "react";
+import SSRApp from "./SSRApp";
 import ProductList from "./components/ProductList";
 import config from "./config.json";
 import axios from "axios";
 
 const App = ({ isSSR, ssrData }) => {
-  const [err, setErr] = useState(false);
-  const [result, setResult] = useState({ loading: true, products: null });
-  useEffect(() => {
-    const getData = async () => {
-      const url = config.SSRApiStack.apiurl;
-      try {
-        let result = await axios.get(url);
-        setResult({ loading: false, products: result.data });
-      } catch (error) {
-        setErr(error);
-      }
-    };
-    getData();
-  }, []);
-  if (err) {
-    return <div>Error {err}</div>;
-  } else {
-    return (
-      <div>
-        <ProductList result={result} />
-      </div>
-    );
-  }
+  return <SSRApp />;
 };
 
 export default App;
